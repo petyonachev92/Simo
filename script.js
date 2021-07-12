5d5e3c9be7fe6d7048a9bcf14594e30199694c61
petyonachev92 <46458941+petyonachev92@users.noreply.github.com>
2021-07-12 10:42:03 +0300

    Fixed

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 function press() {
     let btn = document.getElementsByClassName('btn');
     let img = document.getElementsByClassName('girl');
+    let txt = document.getElementsByClassName('text');
     img[0].hidden = true;
+    
+    txt[0].hidden = true;
 
     btn[0].addEventListener("click", function() {
         if(img[0].hidden) {
             img[0].hidden = false;
+            txt[0].hidden = false;
         } else {
             img[0].hidden = true;
+            txt[0].hidden = true;
         }
     })
 }
